6590c27f114d1e58ff1fc3b2da5a6ad33cf03cf7
sd8956 <duquemoreno12@gmail.com>
2020-08-19 12:24:29 -0500

    Finish image service

--- controllers/Image.js
+++ controllers/Image.js
@@ -1,28 +1,21 @@
 const { upload } = require('../services/Image');
-const singleUpload = upload.single("image")
+const { okResponse, errorResponse } = require('../utils/utils');
+const { errors } = require('../utils/constants');
+const singleUpload = upload.single("image");
 
 // Upload one image to s3
 exports.upload = async (req, res) => {
   try {
     singleUpload(req, res, async function (err) {
-        if (err) {
-          return res.json({
-            success: false,
-            errors: {
-              title: "Image Upload Error",
-              detail: err.message,
-              error: err,
-            },
-          });
-        }
-        let imagePath = { imagePath: req.file.location };
-
-        res.status(201).json({
-            data: imagePath,
-            message: 'image created',
-        });
-      });
+      if (err) {
+        console.log('exports.upload -> err', err);
+        return errorResponse(res, errors.UPLOAD_ERR , err)
+      }
+      let imagePath = { imagePath: req.file.location };
+        
+      okResponse(res, 201, { imagePath })
+    });
 } catch (error) {
-    next(error)
+  next(error)
 }
 }
--- services/Image.js
+++ services/Image.js
@@ -7,19 +7,30 @@ const s3 = new aws.S3();
 aws.config.update({
   secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
   accessKeyId: process.env.S3_ACCESS_KEY_ID,
-  Bucket: 'clotify'
+  Bucket: 'clothify'
 });
 
-exports.upload = multer({
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid file type, only JPEG and PNG is allowed!"), false);
+  }
+};
+
+const upload = multer({
+  fileFilter,
   storage: multerS3({
     acl: "public-read",
     s3,
-    bucket:'clotify',
+    bucket:'clothify',
     metadata: function (req, file, cb) {
       cb(null, { fieldName: "TESTING_METADATA" });
     },
     key: function (req, file, cb) {
-        cb(null, path.basename( file.originalname, path.extname( file.originalname ) ) + '-' + Date.now() + path.extname( file.originalname ) )
+      cb(null, path.basename( file.originalname, path.extname( file.originalname ) ) + '-' + Date.now() + path.extname( file.originalname ) )
     },
   }),
 });
+
+module.exports =  { upload }
